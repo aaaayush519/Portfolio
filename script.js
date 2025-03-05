0936276ec960b6d1a2adf9b6d4c38f2e99db8b93
Ayush Maheshwari <ayushmaheshwari969@gmail.com>
2025-03-05 11:36:25 +0530

    Mobile view fixed

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-    
-if (/Mobi|Android/i.test(navigator.userAgent)) {
-    alert("This website is best viewed in desktop mode. Please enable desktop site mode in your browser.");
-  }
+// if (/Mobi|Android/i.test(navigator.userAgent)) {
+//     alert("This website is best viewed in desktop mode. Please enable desktop site mode in your browser.");
+//   }
   
-  let menuIcon = document.querySelector('#menu-icon');
+let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () =>{
