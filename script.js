e267aa6a9a6158f075667a24fa643e114831dcee
Ayush Maheshwari <ayushmaheshwari969@gmail.com>
2025-05-29 19:51:14 +0530

    Updated on 29 May 2025

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 //     alert("This website is best viewed in desktop mode. Please enable desktop site mode in your browser.");
 //   }
   
+// JavaScript for handling the mobile navigation menu toggle
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () =>{
-    menuIcon.classList.toggle('fa-x')
+// When the menu icon is clicked:
+menuIcon.onclick = () => {
+    // Toggle the 'fa-x' class on the menu icon,
+    // which changes the icon between a menu bars and an 'X'
+    menuIcon.classList.toggle('fa-x');
+
+    // Toggle the 'active' class on the navbar,
+    // which controls its visibility (slide in/out effect via CSS)
     navbar.classList.toggle('active');
 }
